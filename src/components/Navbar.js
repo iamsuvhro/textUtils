9796bfe585051f3b2a97e6baf131130732113e4c
iamsuvhro <suvhradip@gmail.com>
2022-03-23 20:07:15 +0530

    Code snippet of darkmode moved to App.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,10 @@
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 export default function Navbar(props) {
 
-  const [btnText, setbtnText] = useState("Dark mode");
-  const [myStyle,setMyStyle] = useState("light");
-  const [modeColor, setmodeColor] = useState({
-    color : 'black',
-  });
-
-  let toggleStyle = () =>{
-      if (myStyle === "light"){
-      setMyStyle("dark")
-      document.body.style.backgroundColor = 'black';
-      document.body.style.color = 'white';
-      setbtnText("Enable Light mode")
-      setmodeColor({
-        color: 'white',
-      })
-    }
-    else{
-      setMyStyle("light")
-      document.body.style.backgroundColor = 'white';
-      document.body.style.color = 'black';
-      setbtnText("Enable Dark mode")
-      setmodeColor({
-        color: 'black',
-      })
-    }
-  }
-  
   return (
-    <nav className={`navbar navbar-expand-lg navbar-${myStyle} bg-${myStyle}`}>
+    <>
+    <nav className={`navbar navbar-expand-lg navbar-${props.myStyle} bg-${props.myStyle}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -47,13 +21,14 @@ export default function Navbar(props) {
           </ul>
 
           <div className="form-check form-switch mx-3">
-              <input className="form-check-input mx-4 my-3" type="checkbox" onClick={toggleStyle} role="switch" id="flexSwitchCheckChecked" />
-              <label className="form-check-label my-2" htmlFor="flexSwitchCheckChecked" style={modeColor}>{btnText}</label>
+              <input className="form-check-input mx-4 my-3" type="checkbox" onClick={props.toggleStyle} role="switch" id="flexSwitchCheckChecked" />
+              <label className="form-check-label my-2" htmlFor="flexSwitchCheckChecked" style={props.modeColor}>{props.btnText}</label>
           </div>
 
         </div>
       </div>
     </nav>
+    </>
   )
 }
 
