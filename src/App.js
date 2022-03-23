3e766d7f8c0294b8256f3a467123ce186aa9486b
iamsuvhro <suvhradip@gmail.com>
2022-03-23 20:32:00 +0530

    Fixed the undo changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,53 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Body from './components/Body';
 import TextForm from './components/TextForm';
+import showAlert from './components/Navbar';
+import React, {useState} from 'react';
+import Alert from './components/Alert';
 function App() {
+
+  const [btnText, setbtnText] = useState("Dark mode");
+  const [modeColor, setmodeColor] = useState({
+    color : 'black',
+  }); 
+
+  const [myStyle,setMyStyle] = useState("light");
+  let toggleStyle = () =>{
+      if (myStyle === "light"){
+      setMyStyle("dark")
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+      setbtnText("Enable Light mode")
+      setmodeColor({
+        color: 'white',
+      })
+      showAlert('Dark mode hasbeen enabled','success');
+    }
+    else{
+      setMyStyle("light")
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black ';
+      setbtnText("Enable Dark mode")
+      setmodeColor({
+        color: 'black',
+      })
+      showAlert('Light mode hasbeen enabled','success');
+    }
+  }
+    // Creating alert message
+    const [alert, setAlert] = useState(null);
+    const showAlert = (message, type)=>{
+      setAlert({
+        message: message,
+        type: type,
+      })
+    }
   return (
     <>
-    <Navbar title="Text Editor"/>
+    <Navbar title="Text Editor" myStyle={myStyle} modeColor={modeColor} btnText={btnText} toggleStyle={toggleStyle}/>
+    <Alert alert={alert}/>
     <Body/>
-    <TextForm heading="Text Editing Tools"/>
+    <TextForm heading="Text Editing Tools" showAlert={showAlert}/>
     </>
   );
 }
