5ef3969d7657577f353590041d73192a3eeb1cf7
iamsuvhro <suvhradip@gmail.com>
2022-03-25 18:12:42 +0530

    Package updated & UI bugs fixed

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function About() {
   return (
     <>
-    <div className="container">
-        <p>About Us</p>
+    <div className="container my-4">
+        <h2>About Us</h2>
     </div>
     </>
   )
